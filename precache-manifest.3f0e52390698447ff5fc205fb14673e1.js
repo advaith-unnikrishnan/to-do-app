dc74638f8e076da76cfde7019120d9f757e89239
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-07-24 23:02:16 +0530

    Updates

---
+++ precache-manifest.3f0e52390698447ff5fc205fb14673e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea9f81ebd65885084b920ab5d9f41fc",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "b63c26ada742a1a5c8c5",
+    "url": "/to-do-app/static/css/main.387d271b.chunk.css"
+  },
+  {
+    "revision": "954223aceed1d2235682",
+    "url": "/to-do-app/static/js/2.c8b34bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.c8b34bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63c26ada742a1a5c8c5",
+    "url": "/to-do-app/static/js/main.b84710db.chunk.js"
+  },
+  {
+    "revision": "4385e1bd226fdb470057",
+    "url": "/to-do-app/static/js/runtime-main.1f8cf036.js"
+  }
+]);
