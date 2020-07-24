53cd3b3c955191c3374a25f6bfc43203f7ee9325
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-07-24 22:48:48 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import listItems from './ListItems.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput=this.handleInput.bind(this)
+    this.addItem=this.addItem.bind(this)
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault()
+    const newItem=this.state.currentItem
+    console.log(newItem)
+    if (newItem.text!=="") {
+      const newItems=[...this.state.items,newItem]
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter the task" value={this.state.currentItem.text}
+            onChange={this.handleInput}></input>
+            <button type="submit">Add</button>
+          </form>
+          <p>{this.state.items.text}</p>
+          <listItems items={this.state.items}/>
+        </header>
+        
+      </div>
+      
+    )
+  }
 }
 
 export default App;
