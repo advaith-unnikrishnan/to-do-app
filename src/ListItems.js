f7e252602ab4776b8de5e6062dbfe97d953810c5
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-07-29 13:37:52 +0530

    Add trash icon

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './ListItems.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 function ListItems(props){
     const items=props.items
     const listItems=items.map(item =>{
         return <div className="list" key="item.key">
-            <p>{item.text}</p>
+            <p>{item.text}
+                <span>
+                    <FontAwesomeIcon className="icons" icon="trash"></FontAwesomeIcon>
+                </span>
+            </p>
+            
         </div>
     })
     return(
